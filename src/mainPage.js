6792795b8b9b71c02e76179a4e1a6cddef3e927b
hinda <h8565956@gmail.com>
2023-01-10 12:46:04 +0200

    test init value for state

--- src/mainPage.js
+++ src/mainPage.js
@@ -5,11 +5,83 @@ import EditCustomerPage from "./editCustomer";
 import ProductsPage from "./products";
 import Purchases from "./purchases";
 import PurchasByCustomerPage from "./purchasByCustomer";
+import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import firebase from "./firebaseApp";
+import 'firebase/firestore'
+
+
 
 function Main() {
 
+    debugger;
+
+    const dispatch = useDispatch()
+    const [initalValue, setInitalvalue] = useState({ products: [], customers: [], purchases: [] })
+
+    useEffect(() => {
+
+        async function fetchData() {
+        debugger;
+
+            let product = await firebase.firestore().collection('products').get();
+
+            let prodData = [];
+            product.forEach(doc => {
+                let obj = {
+                    id: doc.id,
+                    name: doc.data().name,
+                    price: doc.data().price,
+                    quantity: doc.data().quantity
+                };
+
+                prodData.push(obj)
+            })
+
+            setInitalvalue({ ...initalValue, products: [...initalValue.products, prodData] });
+
+            let customer = await firebase.firestore().collection('customers').get();
+
+            let custData = [];
+            customer.forEach(doc => {
+                let obj = {
+                    id: doc.id,
+                    firstName: doc.data().name,
+                    lastName: doc.data().price,
+                    city: doc.data().quantity
+                };
+
+                custData.push(obj)
+            })
+
+            setInitalvalue({ ...initalValue, customers: [...initalValue.customers, custData] });
+
+
+
+
+            let purchase = await firebase.firestore().collection('purchases').get();
+
+            let purData = [];
+            purchase.forEach(doc => {
+                let obj = {
+                    id: doc.id,
+                    customerId: doc.data().name,
+                    productId: doc.data().price,
+                    date: doc.data().quantity
+                };
+
+                purData.push(obj)
+            })
+
+            setInitalvalue({ ...initalValue, purchases: [...initalValue.purchases, purData] });
+        }
+        fetchData();
+    })
+
+    
     return <div>
 
+        <input type={"button"} value={"loadData"} onClick={()=>dispatch({ type: "LOAD_DATA", payload: initalValue }) } />
         <Link to="/"><h1>Main Page</h1></Link>
 
         <div style={{width:"25%", float:"right"}}>
